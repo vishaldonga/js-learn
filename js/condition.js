51a3f2df8f4c6be9781deb7b606e7acfe4152ce4
Vishal Donga <vdonga@atlassian.com>
2022-09-26 20:04:50 +0530

    Ternary condition

---
+++ js/condition.js
@@ -0,0 +1,27 @@
+//Ternary Operator
+// Conditional Operator
+//Sign : ? :
+//Syntax: (condition) ? true will execute : false will 
+
+//BAsic if-else
+if (5 === 5) { // if condition
+    console.log("true confition")
+} else {
+    console.log("false condition")
+}
+//New syntax=> ternary condtion 
+5 === 5 ? console.log("true confition") : console.log("false condition");
+
+//BASIC IF-elseif-else
+if (5 === 6) {
+    console.log("5==6 confition")
+} else if(5 === 5) {
+    console.log("5==5 confition")
+} else {
+    console.log("else confition")
+}
+
+//New syntax
+5 === 6 ? console.log("5==6 confition") : 5 === 5 ? console.log("5==5 confition") : console.log("else confition")
+
+
